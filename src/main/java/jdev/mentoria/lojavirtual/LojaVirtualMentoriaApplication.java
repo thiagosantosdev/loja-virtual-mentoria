0a8ba8b6296b727f4d1fd3163694a9e6f9912047
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-02 18:59:59 -0300

    Commit

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LojaVirtualMentoriaApplication {
-/*w*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
 	}
