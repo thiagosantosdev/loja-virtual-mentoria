38708dce1d2b7346b325847a54ff71ae7050322e
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-06 22:28:13 -0300

    Endereco

--- src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -1,5 +1,69 @@
 package jdev.mentoria.lojavirtual.model;
 
-public class PessoaJuridica {
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	private String cnpj;
+	private String insEstadual;
+	private String insMunicipal;
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String categoria;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInsEstadual() {
+		return insEstadual;
+	}
+
+	public void setInsEstadual(String insEstadual) {
+		this.insEstadual = insEstadual;
+	}
+
+	public String getInsMunicipal() {
+		return insMunicipal;
+	}
+
+	public void setInsMunicipal(String insMunicipal) {
+		this.insMunicipal = insMunicipal;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
 }
