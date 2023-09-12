f71373f0e682f2efdae14f6336586a77c34f2129
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-11 23:21:57 -0300

    Alteração enum statusContaPagar

--- src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaPagar.java
+++ src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaPagar.java
@@ -5,7 +5,10 @@ public enum StatusContaPagar {
 	COBRANCA("Pagar"),
     VENCIDA("Vencida"),
     ABERTA("Aberta"),
-    QUITADA("Quitada");
+    QUITADA("Quitada"),
+    ALUGUEL("Aluguel"),
+    FUNCIONARIO("Funcionário"),
+    NEGOCIADA("Renegociada");
     
     private String descricao;
     
