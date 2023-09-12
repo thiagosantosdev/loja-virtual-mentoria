bdd80070acfd41fd50bcd44620e9b64fdcf82a34
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-11 23:17:24 -0300

    ContaReceber

---
+++ src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,29 @@
+package jdev.mentoria.lojavirtual.enums;
+
+public enum StatusContaPagar {
+	
+	COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada");
+    
+    private String descricao;
+    
+    private StatusContaPagar(String descricao) {
+    	this.descricao = descricao;
+    }
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+    
+	@Override
+    public String toString() {
+    	return this.descricao;
+    }
+
+}
