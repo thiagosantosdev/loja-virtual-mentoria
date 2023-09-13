1b5d0202e78c945c0e64cbf0eeeaef68f7ab1518
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-13 20:32:37 -0300

    Commit entidade produto

--- src/main/java/jdev/mentoria/lojavirtual/model/CupDesc.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/CupDesc.java
@@ -12,7 +12,6 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.xml.crypto.Data;
 
 @Entity
 @Table(name = "cup_desc")
