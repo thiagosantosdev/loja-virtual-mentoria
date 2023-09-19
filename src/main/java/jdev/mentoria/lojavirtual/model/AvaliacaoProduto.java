b3641c005c914cd137d66cc63e6b66b3ac3fcf69
thiagosantosdev <thiagosantosjss@gmail.com>
2023-09-18 21:20:11 -0300

    Commit Avaliação

--- src/main/java/jdev/mentoria/lojavirtual/model/AvaliacaoProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/AvaliacaoProduto.java
@@ -26,6 +26,8 @@ public class AvaliacaoProduto implements Serializable{
 	
 	private Integer nota;
 	
+	private String descricao;
+	
 
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
@@ -66,6 +68,16 @@ public class AvaliacaoProduto implements Serializable{
 
 	public void setProduto(Produto produto) {
 		this.produto = produto;
+	}
+	
+	
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	@Override
