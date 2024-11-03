783e504a983c3312ccce574da8fcaec0e7d4c961
thiagosantosdev <thiagosantosjss@gmail.com>
2024-11-03 13:10:08 -0300

    Projeto criado

---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/LojaVirtualJdevApplication.java
@@ -0,0 +1,13 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJdevApplication.class, args);
+		System.out.println("Rodouu");
+	}
+}
---
+++ src/test/java/com/mentoria/lojavirtual/LojaVirtualJdev/LojaVirtualJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
