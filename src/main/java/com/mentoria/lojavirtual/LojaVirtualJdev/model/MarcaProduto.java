d5412b0a57cca2ed22b970902d49aafc4892b476
thiagosantosdev <thiagosantosjss@gmail.com>
2024-11-08 21:34:43 -0300

    Pacote model criado

---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/MarcaProduto.java
@@ -0,0 +1,46 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "marca-produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	private Long id_marca_prod;
+	
+	private String nome;
+	
+	
+	
+
+	public Long getId_marca_prod() {
+		return id_marca_prod;
+	}
+
+	public void setId_marca_prod(Long id_marca_prod) {
+		this.id_marca_prod = id_marca_prod;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
