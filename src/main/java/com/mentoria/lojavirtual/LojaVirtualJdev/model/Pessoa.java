e9dc9888695f43a36bcedf6915015e588d156172
thiagosantosdev <thiagosantosjss@gmail.com>
2024-11-16 11:52:03 -0300

    classe PF PJ

---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/Pessoa.java
@@ -0,0 +1,83 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 1, allocationSize = 1)
+public abstract class Pessoa implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+	private Long id_pessoa;
+	private String nome_pessoa;
+	private String email;
+	private String telefone;
+	
+	
+	
+	
+	public Long getId_pessoa() {
+		return id_pessoa;
+	}
+	public void setId_pessoa(Long id_pessoa) {
+		this.id_pessoa = id_pessoa;
+	}
+	public String getNome_pessoa() {
+		return nome_pessoa;
+	}
+	public void setNome_pessoa(String nome_pessoa) {
+		this.nome_pessoa = nome_pessoa;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_pessoa == null) ? 0 : id_pessoa.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id_pessoa == null) {
+			if (other.id_pessoa != null)
+				return false;
+		} else if (!id_pessoa.equals(other.id_pessoa))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/PessoaJuridica.java
@@ -0,0 +1,62 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+public class PessoaJuridica extends Pessoa {
+
+	
+		private static final long serialVersionUID = 1L;
+
+		private String cnpj;
+		private String insc_estadual;
+		private String insc_municipal;
+		private String nome_fantasia;
+		private String razao_social;
+		private String categoria;
+		
+		
+		
+		public String getCnpj() {
+			return cnpj;
+		}
+		public void setCnpj(String cnpj) {
+			this.cnpj = cnpj;
+		}
+		public String getInsc_estadual() {
+			return insc_estadual;
+		}
+		public void setInsc_estadual(String insc_estadual) {
+			this.insc_estadual = insc_estadual;
+		}
+		public String getInsc_municipal() {
+			return insc_municipal;
+		}
+		public void setInsc_municipal(String insc_municipal) {
+			this.insc_municipal = insc_municipal;
+		}
+		public String getNome_fantasia() {
+			return nome_fantasia;
+		}
+		public void setNome_fantasia(String nome_fantasia) {
+			this.nome_fantasia = nome_fantasia;
+		}
+		public String getRazao_social() {
+			return razao_social;
+		}
+		public void setRazao_social(String razao_social) {
+			this.razao_social = razao_social;
+		}
+		public String getCategoria() {
+			return categoria;
+		}
+		public void setCategoria(String categoria) {
+			this.categoria = categoria;
+		}
+		
+		
+		
+		
+}
