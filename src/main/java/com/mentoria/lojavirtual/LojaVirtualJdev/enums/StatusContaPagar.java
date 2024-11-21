7e074a02d88a095bbe9912474e7a1be85b34e21f
thiagosantosdev <thiagosantosjss@gmail.com>
2024-11-20 23:28:20 -0300

    ContaPagar

--- src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/enums/StatusContaPagar.java
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/enums/StatusContaPagar.java
@@ -1,5 +1,26 @@
 package com.mentoria.lojavirtual.LojaVirtualJdev.enums;
 
 public enum StatusContaPagar {
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	NEGOCIADA("Renegociada");
+	
+	private String descricao;
+	
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
 
 }
---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/ContaPagar.java
@@ -0,0 +1,170 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import com.mentoria.lojavirtual.LojaVirtualJdev.enums.StatusContaPagar;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+	private Long id_conta_pagar;
+	
+	private String descricao;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusContaPagar status;
+	
+	@Temporal(TemporalType.DATE)
+	private Date data_vencimento;
+	
+	@Temporal(TemporalType.DATE)
+	private Date data_pagamento;
+	
+	private BigDecimal valor_total;
+	private BigDecimal valor_desc;
+	
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private Pessoa pessoa;
+	
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_forn_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+	private Pessoa pessoa_fornec;
+	
+	
+
+	public Long getId_conta_pagar() {
+		return id_conta_pagar;
+	}
+
+	public void setId_conta_pagar(Long id_conta_pagar) {
+		this.id_conta_pagar = id_conta_pagar;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public StatusContaPagar getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusContaPagar status) {
+		this.status = status;
+	}
+
+	public Date getData_vencimento() {
+		return data_vencimento;
+	}
+
+	public void setData_vencimento(Date data_vencimento) {
+		this.data_vencimento = data_vencimento;
+	}
+
+	public Date getData_pagamento() {
+		return data_pagamento;
+	}
+
+	public void setData_pagamento(Date data_pagamento) {
+		this.data_pagamento = data_pagamento;
+	}
+
+	public BigDecimal getValor_total() {
+		return valor_total;
+	}
+
+	public void setValor_total(BigDecimal valor_total) {
+		this.valor_total = valor_total;
+	}
+
+	public BigDecimal getValor_desc() {
+		return valor_desc;
+	}
+
+	public void setValor_desc(BigDecimal valor_desc) {
+		this.valor_desc = valor_desc;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Pessoa getPessoa_fornec() {
+		return pessoa_fornec;
+	}
+
+	public void setPessoa_fornec(Pessoa pessoa_fornec) {
+		this.pessoa_fornec = pessoa_fornec;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_conta_pagar == null) ? 0 : id_conta_pagar.hashCode());
+		return result;
+	}
+	
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContaPagar other = (ContaPagar) obj;
+		if (id_conta_pagar == null) {
+			if (other.id_conta_pagar != null)
+				return false;
+		} else if (!id_conta_pagar.equals(other.id_conta_pagar))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
