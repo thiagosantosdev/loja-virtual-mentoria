3837a9347c8f2bda8190fe8e93c743fdba367854
thiagosantosdev <thiagosantosjss@gmail.com>
2024-11-27 23:27:03 -0300

    NotaFiscalVenda

---
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/NotaFiscalVenda.java
@@ -0,0 +1,98 @@
+package com.mentoria.lojavirtual.LojaVirtualJdev.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "nota_fiscal_venda")
+@SequenceGenerator(name = "seq_nota_fiscal_venda", sequenceName = "seq_nota_fiscal_venda", initialValue = 1, allocationSize = 1)
+public class NotaFiscalVenda implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_venda")
+	private Long id_nota_fiscal_venda;
+	private String numero;
+	private String serie;
+	private String tipo;
+	@Column(columnDefinition = "text")
+	private String xml;
+	@Column(columnDefinition = "text")
+	private String pdf;
+	
+	
+	
+	public Long getId_nota_fiscal_venda() {
+		return id_nota_fiscal_venda;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public String getSerie() {
+		return serie;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public String getXml() {
+		return xml;
+	}
+	public String getPdf() {
+		return pdf;
+	}
+	public void setId_nota_fiscal_venda(Long id_nota_fiscal_venda) {
+		this.id_nota_fiscal_venda = id_nota_fiscal_venda;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_nota_fiscal_venda == null) ? 0 : id_nota_fiscal_venda.hashCode());
+		return result;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotaFiscalVenda other = (NotaFiscalVenda) obj;
+		if (id_nota_fiscal_venda == null) {
+			if (other.id_nota_fiscal_venda != null)
+				return false;
+		} else if (!id_nota_fiscal_venda.equals(other.id_nota_fiscal_venda))
+			return false;
+		return true;
+	}
+	
+
+}
