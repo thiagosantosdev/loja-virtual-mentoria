7e53274fbf646a839402c251f41d1fa004db52ea
thiagosantosdev <thiagosantosjss@gmail.com>
2025-02-17 14:35:23 -0300

    FlyWay e Profile

--- src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/PessoaFisica.java
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualJdev/model/PessoaFisica.java
@@ -22,7 +22,16 @@ public class PessoaFisica extends Pessoa {
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
 	
+	private String tipo_pessoa;
 	
+	
+	
+	public String getTipo_pessoa() {
+		return tipo_pessoa;
+	}
+	public void setTipo_pessoa(String tipo_pessoa) {
+		this.tipo_pessoa = tipo_pessoa;
+	}
 	public String getCpf() {
 		return cpf;
 	}
